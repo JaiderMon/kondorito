ce6ccfb03228e237db598119c6c2caa07bf5eef9
Jaider <jmonsalve587@unab.edu.co>
2026-05-24 16:48:01 -0500

    Cambios locales

--- inventario.php
+++ inventario.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/conexion.php';
 
+date_default_timezone_set('America/Bogota');
 $fechaSeleccionada = date('Y-m-d');
 
 if(isset($_GET['fecha']) && !empty($_GET['fecha'])){
